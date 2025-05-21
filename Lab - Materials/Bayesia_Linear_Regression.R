81ce2da3a0182a6385db8a122c45d146c0583d21
Diego Andres Perez Ruiz <dapr12@hotmail.com>
2025-05-21 16:11:06 +0100

    Update Bayesia_Linear_Regression.R

--- Lab - Materials/Bayesia_Linear_Regression.R
+++ Lab - Materials/Bayesia_Linear_Regression.R
@@ -73,14 +73,14 @@ y_rep_matrix <- posterior_draws$y_rep
 
 color_scheme_set("brightblue")
 
-ppc_dens_overlay(y = sim_data$y, yrep = y_rep_matrix[1:50, ]) +
-  ggtitle("Posterior Predictive Check: Density Overlay")
+#ppc_dens_overlay(y = sim_data$y, yrep = y_rep_matrix[1:50, ]) +
+#  ggtitle("Posterior Predictive Check: Density Overlay")
 
-ppc_stat(y = sim_data$y, yrep = y_rep_matrix, stat = "mean") +
-  ggtitle("PPC for Mean of y")
+#ppc_stat(y = sim_data$y, yrep = y_rep_matrix, stat = "mean") +
+#  ggtitle("PPC for Mean of y")
 
-ppc_stat(y = sim_data$y, yrep = y_rep_matrix, stat = "sd") +
-  ggtitle("PPC for SD of y")
+#ppc_stat(y = sim_data$y, yrep = y_rep_matrix, stat = "sd") +
+#  ggtitle("PPC for SD of y")
 
 # Posterior regression lines
 alpha_samples <- posterior_draws$alpha
